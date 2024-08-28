e1a39c085575a01ff7e1fbdb67113e6bf22bbb80
Belia <mhfaa152002@gmail.com>
2024-08-28 23:21:02 +0300

    Created Notification Channel function

--- app/src/main/java/com/belia/speedotransfer/MainActivity.kt
+++ app/src/main/java/com/belia/speedotransfer/MainActivity.kt
@@ -1,28 +1,35 @@
 package com.belia.speedotransfer
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.belia.speedotransfer.ui.theme.SpeedoTransferTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        createNotificationChannel(this)
         setContent {
             SpeedoTransferTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
-                }
+                /*TODO
+                   Add appNavigation host to start everything
+                */
             }
         }
     }
+}
+
+private fun createNotificationChannel(context: Context) {
+    val name = "Transactions"
+    val importance = NotificationManager.IMPORTANCE_DEFAULT
+    val channel = NotificationChannel("200", name, importance)
+    channel.description = "Successful Transactions"
+
+    val manager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+    manager.createNotificationChannel(channel)
 }
