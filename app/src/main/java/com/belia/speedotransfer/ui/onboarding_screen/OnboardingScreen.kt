3df7515528311006fc9c54c86e85f3969b91b07a
Belia <mhfaa152002@gmail.com>
2024-09-01 15:15:11 +0300

    Added an Exit button, smalled next button according to the Design

--- app/src/main/java/com/belia/speedotransfer/ui/onboarding_screen/OnboardingScreen.kt
+++ app/src/main/java/com/belia/speedotransfer/ui/onboarding_screen/OnboardingScreen.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
@@ -23,9 +22,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.belia.speedotransfer.R
+import com.belia.speedotransfer.ui.theme.GrayG700
 import com.belia.speedotransfer.ui.theme.GrayG900
 import com.belia.speedotransfer.ui.theme.RedP300
 import kotlinx.coroutines.launch
@@ -38,14 +40,13 @@ fun OnboardingScreen(modifier: Modifier = Modifier) {
     )
     val scope = rememberCoroutineScope()
 
-    BackHandler (enabled = pagerState.currentPage > 0){
+    BackHandler(enabled = pagerState.currentPage > 0) {
         scope.launch {
             pagerState.animateScrollToPage(pagerState.currentPage - 1)
         }
     }
 
     Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Top,
         modifier = modifier
             .fillMaxSize()
@@ -53,12 +54,26 @@ fun OnboardingScreen(modifier: Modifier = Modifier) {
                 brush = Brush.linearGradient(
                     colors = listOf(Color(0xFFFFFFFF), Color(0xFFFFEAEE))
                 )
-            )
+            ),
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
+
+        Button(
+            onClick = {OnBoard()},
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+                .height(50.dp),
+            shape = RoundedCornerShape(6.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = RedP300
+            )
+        ) {
+            Text("Exit")
+        }
+
         TextButton(
-            onClick = {
-                // TODO: Login Screen
-            },
+            onClick = {OnBoard()},
             modifier = modifier
                 .align(Alignment.End)
                 .padding(top = 16.dp)
@@ -66,55 +81,25 @@ fun OnboardingScreen(modifier: Modifier = Modifier) {
         ) {
             Text(
                 text = "Skip",
-                color = GrayG900
+                color = RedP300,
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold
             )
         }
-        HorizontalPager(
-            state = pagerState,
-            verticalAlignment = Alignment.Top,
-            //pageSize = PageSize.Fill,
-            modifier = modifier
-                .fillMaxWidth()
-                //.weight(1f)
-                .padding(16.dp)
-        ) { page ->
-            when (page) {
-                0 -> OnBoardingScreenDesign(
-                    image = R.drawable.loading_rafiki,
-                    title = R.string.amount,
-                    description = R.string.amount_description,
-                    pagerState
-                )
-                1 -> OnBoardingScreenDesign(
-                    image = R.drawable.currency_rafiki,
-                    title = R.string.confirmation,
-                    description = R.string.confirmation_description,
-                    pagerState
-                )
-                2 -> OnBoardingScreenDesign(
-                    image = R.drawable.payments_rafiki,
-                    title = R.string.payment,
-                    description = R.string.payment_description,
-                    pagerState
-                )
-            }
-        }
+        OnBoardingScreenDesign(
+            image = R.drawable.loading_rafiki,
+            title = R.string.amount,
+            description = R.string.amount_description,
+            pagerState
+        )
+
+
         Button(
-            onClick = {
-                if (pagerState.currentPage < 2) {
-                    scope.launch {
-                        pagerState.animateScrollToPage(pagerState.currentPage + 1)
-                    }
-                } else {
-                    // Navigate to the next screen or close onboarding
-                    // TODO: Login Screen
-                }
-            },
+            onClick = {OnBoard()},
             modifier = Modifier
-                .padding(16.dp)
-                .fillMaxWidth()
-                .height(50.dp),
-            shape = RoundedCornerShape(6.dp),
+                .padding(8.dp)
+                .height(30.dp),
+            shape = RoundedCornerShape(12.dp),
             colors = ButtonDefaults.buttonColors(
                 containerColor = RedP300
             )
@@ -130,6 +115,6 @@ private fun OnboardingScreenPrev() {
     OnboardingScreen()
 }
 
-fun OnBoard(){
+fun OnBoard() {
     Log.d("trace", "OnBoard")
 }
