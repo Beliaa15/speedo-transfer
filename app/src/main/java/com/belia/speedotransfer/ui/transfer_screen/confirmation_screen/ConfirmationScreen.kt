0b726a3208056d9e961e808fdbb59b066f7d7ca7
mohaned-emad <mohanedemad11@gmail.com>
2024-09-07 17:33:33 +0300

    Confirmation Screen built

---
+++ app/src/main/java/com/belia/speedotransfer/ui/transfer_screen/confirmation_screen/ConfirmationScreen.kt
@@ -0,0 +1,105 @@
+package com.belia.speedotransfer.ui.transfer_screen.confirmation_screen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.belia.speedotransfer.R
+import com.belia.speedotransfer.ui.common_ui.EmptyButton
+import com.belia.speedotransfer.ui.common_ui.RedButton
+import com.belia.speedotransfer.ui.common_ui.StepProgressIndicator
+import com.belia.speedotransfer.ui.common_ui.TopBar
+import com.belia.speedotransfer.ui.common_ui.TotalAmount
+import com.belia.speedotransfer.ui.common_ui.TransferSection
+import com.belia.speedotransfer.ui.theme.GrayG500
+import com.belia.speedotransfer.ui.theme.GrayG900
+import com.belia.speedotransfer.ui.theme.bodyRegular16
+import com.belia.speedotransfer.ui.theme.titleSemiBold
+
+@Composable
+fun ConfirmationScreen(
+    amount: Float,
+    name: String,
+    account: String,
+    navController: NavController,
+    modifier: Modifier = Modifier
+) {
+    Scaffold (
+        topBar = {
+            TopBar(color = Color(0xFFFFF8E7), hasIcon = true, title = "Transfer")
+        }
+    ) {
+            innerPadding ->
+        Column (
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = modifier
+                .fillMaxSize()
+                .background(
+                    brush = Brush.linearGradient(
+                        colors = listOf(Color(0xFFFFF8E7), Color(0xFFFFEAEE)),
+                    )
+                )
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+        ) {
+            StepProgressIndicator(currentStep = 2)
+            Spacer(modifier = modifier.padding(12.dp))
+            Text(
+                text = "${amount.toInt()} EGP",
+                style = titleSemiBold,
+                color = GrayG900,
+                modifier = modifier.padding(8.dp)
+            )
+            Text(
+                text = "Transfer amount",
+                style = bodyRegular16,
+                color = GrayG500,
+                modifier = modifier.padding(4.dp)
+            )
+            TotalAmount(amount = amount)
+
+            TransferSection(
+                fromName = "Asmaa Dosuky",
+                fromAccountNum = "xxxx7890",
+                toName = name,
+                toAccountNum = account,
+                image = R.drawable.ic_transfer
+            )
+
+            RedButton(
+                text = "Confirm",
+                onClick = {
+                    // navController.navigate("$PAYMENT/$amount/$name/$account")
+                    // TODO: Add navigation to payment screen
+                }
+            )
+            EmptyButton(
+                text = "Previous",
+                onClick = {
+                    // navController.navigate(AMOUNT)
+                    // TODO: Add navigation to amount screen
+                }
+            )
+        }
+    }
+}
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+private fun TransferScreenPreview() {
+    ConfirmationScreen(amount = 1000f, "Jonathan Smith", "xxxx7890", rememberNavController())
+}
