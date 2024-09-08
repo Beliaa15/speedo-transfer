c8aa6a7754f5476a5fc7d4fba48183d2a4764b2f
mohaned-emad <mohanedemad11@gmail.com>
2024-09-08 12:45:38 +0300

    Added scroll functionality to Transaction Screen

--- app/src/main/java/com/belia/speedotransfer/ui/transactions_screen/transaction/Transaction.kt
+++ app/src/main/java/com/belia/speedotransfer/ui/transactions_screen/transaction/Transaction.kt
@@ -6,6 +6,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -57,6 +59,7 @@ fun Transaction(
                     )
                 )
                 .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
         ) {
             Image(
                 painter = painterResource(id = R.drawable.ic_confirm),
