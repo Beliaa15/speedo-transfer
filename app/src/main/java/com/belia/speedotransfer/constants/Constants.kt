1fb771519a20ebfde27fe9c027ba5d4865ed4637
Belia <mhfaa152002@gmail.com>
2024-09-28 15:55:38 +0300

    Updated new endpoint for USER_ENDPOINT

--- app/src/main/java/com/belia/speedotransfer/constants/Constants.kt
+++ app/src/main/java/com/belia/speedotransfer/constants/Constants.kt
@@ -3,10 +3,11 @@ package com.belia.speedotransfer.constants
 object Constants {
     // Waiting for BE team to send userId and deploy
     const val BASE_URL = "https://speedo-api.vercel.app/"
+    //const val BASE_URL = "http://10.0.2.2:3000/api/v1/"
     const val LOGIN_ENDPOINT = "auth/login"
     const val REGISTER_ENDPOINT = "auth/register"
     const val LOGOUT_ENDPOINT = "auth/logout"
-    const val USER_ENDPOINT = "account/{customerId}"
+    const val USER_ENDPOINT = "user/getUser"
     const val CHANGE_PASSWORD_ENDPOINT = "customers/password/{customerId}"
     const val EDIT_PROFILE_ENDPOINT = "customers/data/{customerId}"
     const val CREATE_FAVOURITE_ENDPOINT = "favourites/{accountNumber}"
